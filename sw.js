f71d39d997b42ed92e46e3792c18bfd1804202e0
michaeldiaz5493-png <michaeldiaz5493@gmail.com>
2026-03-15 21:57:38 -0500

    Add files via upload
    Actualización

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'passionplay-v5';
+const CACHE = 'passionplay-v6';
 const FILES = [
   './',
   './index.html',
@@ -6,33 +6,18 @@ const FILES = [
   './icon-192.png',
   './icon-512.png'
 ];
-
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
   if(e.request.mode === 'navigate') {
-    e.respondWith(
-      fetch(e.request)
-        .then(res => { const c = res.clone(); caches.open(CACHE).then(ca => ca.put(e.request, c)); return res; })
-        .catch(() => caches.match(e.request) || caches.match('./index.html'))
-    );
+    e.respondWith(fetch(e.request).then(res => { const c = res.clone(); caches.open(CACHE).then(ca => ca.put(e.request,c)); return res; }).catch(() => caches.match(e.request) || caches.match('./index.html')));
   } else {
-    e.respondWith(
-      fetch(e.request)
-        .then(res => { const c = res.clone(); caches.open(CACHE).then(ca => ca.put(e.request, c)); return res; })
-        .catch(() => caches.match(e.request))
-    );
+    e.respondWith(fetch(e.request).then(res => { const c = res.clone(); caches.open(CACHE).then(ca => ca.put(e.request,c)); return res; }).catch(() => caches.match(e.request)));
   }
 });
