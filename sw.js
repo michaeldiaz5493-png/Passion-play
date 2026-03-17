c5c49b159435c87670d97f2a83b7242ad3d68715
michaeldiaz5493-png <michaeldiaz5493@gmail.com>
2026-03-17 15:12:54 -0500

    Add files via upload
    Actualizacion

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-const CACHE = 'passionplay-v27';
+const CACHE = 'passionplay-v29';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))); self.skipWaiting(); });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch', e => {
   if (e.request.mode === 'navigate') {
     e.respondWith(fetch(e.request).then(res => { const c = res.clone(); caches.open(CACHE).then(ca => ca.put(e.request, c)); return res; }).catch(() => caches.match(e.request) || caches.match('./index.html')));
